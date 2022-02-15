27aca4cbdd4393ddb4db3c873f1a1a513b54e198
nao <b1018080@fun.ac.jp>
2022-02-15 11:20:52 +0900

    make background

---
+++ Application/main.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+from tkinter.constants import LEFT, NUMERIC
+import cv2
+import PIL.Image, PIL.ImageTk
+from PIL import Image, ImageTk, ImageOps
+import datetime
+import os
+import serial
+
+class Application(tk.Frame):
+    def __init__ (self, master=None):
+        super().__init__(master)
+        self.master.geometry("720x450")
+        self.master.title("FudeTasrou")
+        self.master.resizable(width=False, height=False)
+
+        self.createFrame()
+
+    def createFrame(self):
+        self.background=tk.Frame(self.master, width=720, height=450, bg="#C4C4C4")
+        self.background.place(x=0, y=0)
+
+def main():
+    root=tk.Tk()
+    app=Application(master = root)
+    app.mainloop()
+
+if __name__=="__main__":
+    main()
