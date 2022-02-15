769cae4034156a82ca98a33ffcb4a39081876585
nao <b1018080@fun.ac.jp>
2022-02-15 17:31:09 +0900

    road serial

--- Application/readSerial.py
+++ Application/readSerial.py
@@ -2,15 +2,28 @@ import string
 import serial
 from threading import Thread
 import math
+import os
 
 class getAngle(Thread):
-    def __init__(self, port:str):
+    def __init__(self):
+        super().__init__()
         self.accX:float=0.0
         self.accY:float=0.0
         self.accZ:float=0.0
 
-        self.ser = serial.Serial(port, 9600, timeout=None)
-        
+        self.rawData = [0, 0, 0]
+
+        self.ser = serial.Serial()
+        self.ser.baudrate = 9600
+        '''
+        for file in os.listdir("/dev"):
+            if "tty.usbmodem" in file:
+                self.ser.port = "/dev/"+file
+                self.ser.open()
+        '''
+
+        self.ser.port = "/dev/cu.usbserial-14330"
+        self.ser.open()
         #line=0.0 0.0 0.0 0.0 0.0 0.0
 
 
@@ -18,10 +31,15 @@ class getAngle(Thread):
         try:
             while True:
                 line=self.ser.readline()
-                rowData=line.split()
-                self.EulerAngle(accX=rowData[0], accY=rowData[1], accZ=rowData[2])
-        except:
+                self.rawData=line.split()
+                self.rawData = [int(i) for i in self.rawData]
+                #print(rawData)
+                #self.EulerAngle(accX=rowData[0], accY=rowData[1], accZ=rowData[2])
+        except Exception as e:
+            import traceback
+            print(traceback.format_exc())
             return
+
 
 
     def EulerAngle(self, accX:float, accY:float, accZ:float)->list:
