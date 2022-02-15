699f395c8e74c8b9883da4ccfd007b3bfd1e3e7c
gawa-K <kitogawa@g.ecc.u-tokyo.ac.jp>
2022-02-15 15:29:10 +0900

    add force

---
+++ Fude/force/force.ino
@@ -0,0 +1,21 @@
+
+const int FORCE_PIN = A0;
+
+void setup(){
+    Serial.begin( 9600 );
+}
+
+void loop(){
+    int value;
+    float volt;
+
+    value = analogRead( FORCE_PIN );
+
+    volt = value * 5.0 / 1023.0;
+
+    Serial.print( "Value: " );
+    Serial.print( value );
+    Serial.print( "  Volt: " );
+    Serial.println( volt );
+    delay( 100 );
+}
