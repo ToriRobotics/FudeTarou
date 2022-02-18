da5f7e012865dbcc8fae17876dbdcd52037b1c3a
nao <b1018080@fun.ac.jp>
2022-02-18 15:20:52 +0900

    fix shear

--- Application/viewStressFrame.py
+++ Application/viewStressFrame.py
@@ -18,9 +18,11 @@ class StressFrame():
 
          #stress 0->255
         cv2.circle(self.img, (int(self.width/2), int(self.height/2)), 3, (0,0,0), thickness=-1)
-        stress=min(stress*3,255)
-        #cv2.circle(self.img, (int(self.width/2) + shearX, int(self.height/2) - shearY), 20, (255 -stress, 255 - stress, 200), thickness=-1)
-        cv2.circle(self.img, (int(self.width/2), int(self.height/2)), 20, (255 - stress, 255 - stress, 255), thickness=-1)
+        stress=min(stress*3,235)
+        shearX=min(shearX*3,640)
+        shearY=min(shearY*3,640)
+        cv2.circle(self.img, (int(self.width/2 + shearX), int(self.height/2 - shearY)), 15, (235 -stress, 235 - stress, 235), thickness=-1)
+        #cv2.circle(self.img, (int(self.width/2), int(self.height/2)), 20, (255 - stress, 255 - stress, 255), thickness=-1)
         self.imgResize=cv2.resize(self.img, dsize=(widthD, widthD))
         self.imgRGB=cv2.cvtColor(self.imgResize, cv2.COLOR_BGR2RGB)
 
