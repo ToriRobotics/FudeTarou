f754a4b1094ddb9345b03c3eb9d057473712b9a4
nao <b1018080@fun.ac.jp>
2022-02-19 13:59:46 +0900

    add button

--- Application/main.py
+++ Application/main.py
@@ -5,6 +5,7 @@ import PIL.Image, PIL.ImageTk
 from PIL import Image, ImageTk, ImageOps
 import datetime
 import os
+from numpy import pad
 import serial
 import readSerial
 #import makeFudeFrame
@@ -29,14 +30,24 @@ class Application(tk.Frame):
         self.angleFrame=viewAngleFrame.AngleFrame()
 
         self.showFrame()
+        self.createWidget()
 
     def createFrame(self):
-        self.background=tk.Frame(self.master, width=1280, height=720, bg="#C4C4C4")
-        self.background.place(x=0, y=0)
+        #self.background=tk.Frame(self.master, width=1280, height=720, bg="#C4C4C4")
+        #self.background.place(x=0, y=0)
+
+        self.buttonFrame=tk.Frame(self.master, width=1280, height=40, bg="#262626")
+        self.buttonFrame.place(x=0, y=0)
     
+    def createWidget(self):
+        button_FudeTracking=tk.Button(self.buttonFrame, text="tracking", width=3)
+        button_FudeTracking.grid(row=0, column=0, padx=0, pady=0, sticky=tk.E)
+        button_endApp=tk.Button(self.buttonFrame, text="exit", width=3, command=self.exitApp)
+        button_endApp.grid(row=0, column=1, padx=0, pady=0, sticky=tk.E)
+
     def showFrame(self):
         self.stressCanvas=tk.Canvas(self.master, width=640, height=720)
-        self.stressCanvas.place(x=0, y=0)
+        self.stressCanvas.place(x=0, y=30)
         
         self.gripCanvas=tk.Canvas(self.master, width=640, height=360)
         self.gripCanvas.place(x=640, y=0)
@@ -59,16 +70,11 @@ class Application(tk.Frame):
         self.angleimgPIL=PIL.Image.fromarray(self.angleFrame.imgRGB)
         self.angleimgTk=PIL.ImageTk.PhotoImage(self.angleimgPIL)
         self.angleCanvas.create_image(0, 0, image=self.angleimgTk, anchor="nw")
-        
-        '''
-        self.canvas = tk.Canvas(self.master, width=self.fudeFrame.widthD, height=self.fudeFrame.heightD)
-        #canvasの位置
-        self.canvas.place(x=0, y=0)
-        #canvas内の画像の位置
-        self.canvas.create_image(0, 0, image = self.imgTk, anchor="nw")
-        '''
 
         self.master.after(100, self.showFrame)
+
+    def exitApp(self):
+        self.master.destroy()
 
 def main():
     root=tk.Tk()
