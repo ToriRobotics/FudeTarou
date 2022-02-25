13eeb72d1d3a267ee61b89e2550ad22d30fc4d8f
Ishibashi Kyosuke <ishibashi-kyosuke328@g.ecc.u-tokyo.ac.jp>
2022-02-25 17:31:10 +0900

    change graph layout

--- Application/makeGraph.py
+++ Application/makeGraph.py
@@ -29,28 +29,39 @@ def make_graph(test_path, ans_path):
     shearY_ans = ans_data.get_data("shearY")
     grip_ans = ans_data.get_data("grip")
     angle_ans = ans_data.get_data("angle")
-
     
-    fig = plt.figure()
+    fig = plt.figure(figsize=(10,10))
+    fig.subplots_adjust(hspace=1)
+
     ax1 = fig.add_subplot(5,1,1)
-    ax1.plot(np.linspace(0,99,100), signal.resample(stress_test, 100), marker="None")
-    ax1.plot(np.linspace(0,99,100), signal.resample(stress_ans, 100), marker="None")
+    ax1.plot(np.linspace(0,99,100), signal.resample(stress_ans, 100), marker="None", label="answer")
+    ax1.plot(np.linspace(0,99,100), signal.resample(stress_test, 100), marker="None", label="result", lw=3)
+    ax1.set_title("stress", fontsize=20)
+    ax1.legend()
     
     ax2 = fig.add_subplot(5,1,2)
-    ax2.plot(np.linspace(0,99,100), signal.resample(shearX_test, 100), marker="None")
-    ax2.plot(np.linspace(0,99,100), signal.resample(shearX_ans, 100), marker="None")
+    ax2.plot(np.linspace(0,99,100), signal.resample(shearX_ans, 100), marker="None", label="answer")
+    ax2.plot(np.linspace(0,99,100), signal.resample(shearX_test, 100), marker="None", label="result", lw=3)
+    ax2.set_title("shearX", fontsize=20)
+    ax2.legend()
 
     ax3 = fig.add_subplot(5,1,3)
-    ax3.plot(np.linspace(0,99,100), signal.resample(shearY_test, 100), marker="None")
-    ax3.plot(np.linspace(0,99,100), signal.resample(shearY_ans, 100), marker="None")
+    ax3.plot(np.linspace(0,99,100), signal.resample(shearY_ans, 100), marker="None", label="answer")
+    ax3.plot(np.linspace(0,99,100), signal.resample(shearY_test, 100), marker="None", label="result", lw=3)
+    ax3.set_title("shearY", fontsize=20)
+    ax3.legend()
 
     ax4 = fig.add_subplot(5,1,4)
-    ax4.plot(np.linspace(0,99,100), signal.resample(grip_test, 100), marker="None")
-    ax4.plot(np.linspace(0,99,100), signal.resample(grip_ans, 100), marker="None")
+    ax4.plot(np.linspace(0,99,100), signal.resample(grip_ans, 100), marker="None", label="answer")
+    ax4.plot(np.linspace(0,99,100), signal.resample(grip_test, 100), marker="None", label="result", lw=3)
+    ax4.set_title("grip", fontsize=20)
+    ax4.legend()
 
     ax5 = fig.add_subplot(5,1,5)
-    ax5.plot(np.linspace(0,99,100), signal.resample(angle_test, 100), marker="None")
-    ax5.plot(np.linspace(0,99,100), signal.resample(angle_ans, 100), marker="None")
+    ax5.plot(np.linspace(0,99,100), signal.resample(angle_ans, 100), marker="None", label="answer")
+    ax5.plot(np.linspace(0,99,100), signal.resample(angle_test, 100), marker="None", label="result", lw=3)
+    ax5.set_title("angle", fontsize=20)
+    ax5.legend()
 
     return fig
     
